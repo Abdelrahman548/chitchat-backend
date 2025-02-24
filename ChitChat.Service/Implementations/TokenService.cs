e8b18954d4503debcffac0aa60ef1049a8c78d5e
Abdelrahman Amr <abdelrahmanamr68@gmail.com>
2025-02-24 19:01:21 +0200

    implement token service

---
+++ ChitChat.Service/Implementations/TokenService.cs
@@ -0,0 +1,55 @@
+﻿using ChitChat.Data.Entities;
+using ChitChat.Service.Helpers;
+using ChitChat.Service.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System;
+using System.Security.Cryptography;
+
+namespace ChitChat.Service.Implementations
+{
+    public class TokenService : ITokenService
+    {
+        private readonly JwtOptions jwtOptions;
+
+        public TokenService(JwtOptions jwtOptions)
+        {
+            this.jwtOptions = jwtOptions;
+        }
+        public string GenerateAccessToken(User person)
+        {
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = jwtOptions.Issuer,
+                Audience = jwtOptions.Audience,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SigningKey)),
+                                    SecurityAlgorithms.HmacSha256),
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new(ClaimTypes.NameIdentifier, person.Id.ToString()),
+                    new(ClaimTypes.Name, person.Email),
+                    new(ClaimTypes.Role, "User"),
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(jwtOptions.LifeTime),
+            };
+
+            // Generate Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            var accessToken = tokenHandler.WriteToken(securityToken);
+            return accessToken;
+        }
+
+        public string GenerateRefreshToken()
+        {
+            var randomBytes = new byte[64];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomBytes);
+            }
+            return Convert.ToBase64String(randomBytes);
+        }
+    }
+}
