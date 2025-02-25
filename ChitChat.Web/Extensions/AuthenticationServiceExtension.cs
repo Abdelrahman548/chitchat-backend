9efcb9d47c78d560710d4c833367311e3d2a4900
Abdelrahman Amr <abdelrahmanamr68@gmail.com>
2025-02-25 18:47:30 +0200

    add bearer authentication

--- ChitChat.Web/Extensions/AuthenticationServiceExtension.cs
+++ ChitChat.Web/Extensions/AuthenticationServiceExtension.cs
@@ -1,4 +1,7 @@
 ﻿using ChitChat.Service.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace ChitChat.Web.Extensions
 {
@@ -9,7 +12,26 @@ namespace ChitChat.Web.Extensions
             var jwtOptions = configuration.GetSection("Jwt").Get<JwtOptions>();
             if (jwtOptions is not null)
             {
+                services.AddAuthentication()
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.SaveToken = true;
 
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtOptions.Issuer,
+
+                        ValidateAudience = true,
+                        ValidAudience = jwtOptions.Audience,
+
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SigningKey)),
+
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero,
+                    };
+                });
             }
         }
     }
